e666f7bfefdf83227cb3f325792f9dd3b6df9ab7
aujlarj <aujla.rj@hotmail.com>
2020-05-26 14:03:24 -0700

    inital setup done, scraping each indiviual site

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
 #from webdriver_manager.chrome import ChromeDriverManager
 import requests
 
@@ -59,8 +60,8 @@ def get_main_url():
     # search_keywords, city, state, country = user_input()
 
     country = 'US'
-    search_keywords = 'Data Science'
-    city = 'san francisco'
+    search_keywords = 'Data Scienctist'
+    city = 'San Francisco'
     state = 'CA'
 
     search_keywords = search_keywords.strip()
@@ -83,10 +84,11 @@ def get_main_url():
     return website
 
 
-def get_eachjob_url(job_site, num_jobs):
+def get_eachjob_url(job_site):
     job_counter = 0
     next_url_counter = 0
-    jobs_urls_ids = []
+    jobs_urls_jks = []
+    # jobcard_div_ids = [] # undo comment on all the id variable to automate onpage jobcarb scraping
     url_flag = True
 
     chrome_options = Options()
@@ -94,31 +96,54 @@ def get_eachjob_url(job_site, num_jobs):
     driver = webdriver.Chrome(
         chrome_options=chrome_options, executable_path=CHROME_PATH)
 
-    while(job_counter < num_jobs and url_flag):
+    # driver.delete_all_cookies()
+
+    driver.implicitly_wait(30)
+
+    while(url_flag):
         new_site = job_site + '&start=' + str(next_url_counter)
         print(new_site)
 
         driver.get(new_site)
-        driver.implicitly_wait(5)
-        divs = driver.find_elements_by_class_name('result')
+        # can not have space in by_class_name. Must be one word
+        divs = driver.find_elements_by_class_name('jobsearch-SerpJobCard')
 
         job_check = 0
         for div in divs:
-            job_id = div.get_attribute('data-jk')
-            if job_id not in jobs_urls_ids:
-                jobs_urls_ids.append(job_id)
+            job_jk = div.get_attribute('data-jk')
+            # div_id = div.get_attribute('id')
+            if job_jk not in jobs_urls_jks:
+                jobs_urls_jks.append(job_jk)
+                # jobcard_div_ids.append(div_id)
                 job_counter += 1
                 job_check += 1
+                # # Automate click on each Job carb(useful if it shows the onpage description)
+                # # not consistent tho
+                # element_to_click = driver.find_element_by_id(div_id)
+                # ActionChains(driver).click(element_to_click).perform()
+
+                # # Explaination
+                # # Create the object for Action Chains
+                # actions = ActionChains(driver)
+                # actions.click(element_to_click)
+                # # perform the operation on the element
+                # actions.perform()
+                # # Can also do this: element_to_click.click()
+
                 print('Job_counter:', job_counter)
 
+        # Exit if no new jobs found
         if job_check == 0:
             url_flag = False
 
         next_url_counter += 10
 
     driver.close()
+    return jobs_urls_jks
 
-    return jobs_urls_ids
+
+def scrape_each_job(url_jks):
+    pass
 
 
 def get_total_num_jobs(job_site):
@@ -154,12 +179,18 @@ def open_browser(url):
 
 def main():
     main_url = get_main_url()
-    # main_url = 'https://www.indeed.com/jobs?q=Data+Scientist+%24157%2C000&l=San+Francisco%2C+CA&radius=0'
-    number_of_jobs = get_total_num_jobs(main_url)
-    print('Number of Jobs:', number_of_jobs)
+
+    # testing('https://www.indeed.com/jobs?q=Data+Scientist+%24157%2C000&l=San+Francisco%2C+CA&radius=0')
+
+    # number_of_jobs = get_total_num_jobs(main_url)
+    # print('Number of Jobs:', number_of_jobs)
     # open_browser(main_url)  # for testing
-    job_urls = get_eachjob_url(main_url, number_of_jobs)
-    write_to_new_file('url.txt', job_urls)
+
+    job_url_jks = get_eachjob_url(main_url)
+    scrape_each_job(job_url_jks)
+
+    # click_eachjob(main_url)
+
     # pprint(urls)
     # print(len(urls))
 
@@ -174,38 +205,3 @@ if __name__ == "__main__":
 #     attribute_name = 'data-jk'
 #     job_counter = 0
 #     next_url_counter = 10
-
-#     count_div = firstpage.find(id='searchCountPages')
-#     unpack_div_string = count_div.contents[0]
-#     unpack_div_string = unpack_div_string.strip()
-#     unpack_div_list = unpack_div_string.split(" ")
-
-#     if int(unpack_div_list[3]):
-#         num_jobs = int(unpack_div_list[3])
-#     else:
-#         print('ERROR! Unpacking Div for Number of jobs not working')
-#     # print(num_jobs)
-
-#     jobsite_divs = firstpage.findAll(
-#         "div", class_="jobsearch-SerpJobCard unifiedRow row result")
-
-#     for div in jobsite_divs:
-#         # can also use div.get(attribute_name)
-#         all_urls.append(base_url+div[attribute_name])
-#         job_counter += 1
-
-#     while(job_counter <= num_jobs):
-#         new_site = job_site + '&start=' + str(next_url_counter)
-#         print(new_site)
-#         new_source = requests.get(new_site).text
-#         next_page = BeautifulSoup(new_source, 'html.parser')
-
-#         jobsite_divs = next_page.findAll(
-#             "div", class_="jobsearch-SerpJobCard unifiedRow row result")
-
-#         for div in jobsite_divs:
-#             # can also use div.get(attribute_name)
-#             all_urls.append(base_url+div[attribute_name])
-#             job_counter += 1
-
-#         next_url_counter += 10
